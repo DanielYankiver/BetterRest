9e3df671562530b4205e2a19380d904d796a1972
Daniel Yankiver <dyankiver@gmail.com>
2023-12-18 11:04:00 -0500

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Daniel Yankiver on 12/18/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
