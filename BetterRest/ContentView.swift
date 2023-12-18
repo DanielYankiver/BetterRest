12ba67ff233cb82123092333b0affb9a85c743c0
Daniel Yankiver <dyankiver@gmail.com>
2023-12-18 13:14:41 -0500

    dates and machine learning models

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -8,17 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+  var body: some View {
+    Text(Date.now, format: .dateTime.hour().minute())
+    Text(Date.now, format: .dateTime.day().month().year())
+    Text(Date.now.formatted(date: .long, time: .complete))
+  }
+
+  func exampleDates() {
+//    var components = DateComponents()
+//    components.hour = 8
+//    components.minute = 0
+//    let date = Calendar.current.date(from: components) ?? .now
+
+    let components = Calendar.current.dateComponents([.hour, .minute], from: .now)
+    let hour = components.hour ?? 0
+    let minute = components.minute ?? 0
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }
